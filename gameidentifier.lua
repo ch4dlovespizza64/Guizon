bbb15d963e2506334df57e652946ef378861f15f
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2023-06-27 21:22:48 +0800

    Update gameidentifier.lua

--- gameidentifier.lua
+++ gameidentifier.lua
@@ -3,11 +3,11 @@ if game.PlaceId == 189707 then
 	  Title = "GUIZON LOADED",
 	  Text = "Game: Natural Disasters Survival",
   })
-  loadstring(game:HttpGet("ComingSoon"))() -- Natural Disasters Survival
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/Guizon/main/games/nds.lua"))() -- Natural Disasters Survival
 else
     game:GetService("StarterGui"):SetCore("SendNotification",{
 	  Title = "WARNING", -- Required
 	  Text = "Game is not whitelisted! Loading Universal",
   })
-  loadstring(game:HttpGet("UniversalComingSoon"))() -- Universal
+  game.Players.LocalPlayer:Kick("Sorry for kicking you out of the game\nThe game your currently playing isn't whitelisted!")
 end
