f78ac5fdd92cf35a48e00bceadbcccb54ac05d38
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2023-06-28 18:57:49 +0800

    Update gameidentifier.lua

--- gameidentifier.lua
+++ gameidentifier.lua
@@ -4,6 +4,12 @@ if game.PlaceId == 189707 then
 	  Text = "Game: Natural Disasters Survival",
   })
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/Guizon/main/games/nds.lua"))() -- Natural Disasters Survival
+elseif game.PlaceId == 2413927524 then
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+	  Title = "GUIZON LOADED",
+	  Text = "Game: The Rake: Remastered",
+  })
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/Guizon/main/games/the-rake-remastered.lua"))()
 else
     game:GetService("StarterGui"):SetCore("SendNotification",{
 	  Title = "WARNING", -- Required
